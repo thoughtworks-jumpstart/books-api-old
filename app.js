291cbbee00a73173be3b3b1fab4339b91d75d554
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-25 15:53:14 +0800

    Only allow heroku UI URL in production

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const books = require("./routes/books.js");
 const authors = require("./routes/authors.js");
 
 const app = express();
-app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: process.env.ALLOWED_ORIGIN
+    })
+  );
+} else {
+  app.use(cors());
+}
 app.use(logger("dev"));
 app.use(express.json());
 
