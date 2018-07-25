ef541ce7fa1e897a2ed16157ef8b626bfc9982ab
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-25 14:36:21 +0800

    Enable CORS

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
 
 const index = require("./routes/index");
 const books = require("./routes/books.js");
 const authors = require("./routes/authors.js");
 
 const app = express();
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 
